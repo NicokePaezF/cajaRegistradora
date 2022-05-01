55045d10da313617867bf804aacdf50167a16c70
Nicoke <jpaezf@gmail.com>
2022-04-30 23:18:56 -0400

    main.py: boleta terminada

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import datetime
+# {}: dictionary
+# []: list
 
-boleta = {}     #Diccionario
-rut_cliednte = input("Ingrese rut del Cliente, para asociarlo a sus puntos:")
-correo_cliente = input("Ingrese correo del cliente para su boleta virtual:")
-fecha_actual = datetime.date.today()
-boleta['rut_cliente'] = rut_cliednte
-boleta['correo_cliente'] = correo_cliente
-boleta['fecha_actual'] = fecha_actual
+invoice = {}
+rut_customer = input("Enter RUT customer, to associate them with their points:")
+mail_customer = input("Enter customer email for your virtual ticket:")
+date_now = datetime.date.today()
+invoice['rut_customer'] = rut_customer
+invoice['mail_customer'] = mail_customer
+invoice['date_now'] = date_now
+invoice['details'] = []
+print('Start product entry: ')
 
-print(boleta)
+state = True
+while state:
+    code_product = input('Enter the product code: ')
+    name_product = input('Enter the product name: ')
+    category = input('Enter the product category : ')
+    price = input('Enter the product price : ')
+    amount = input('Enter the product amount: ')
+    details = {
+        'code_product': code_product,
+        'name_product': name_product,
+        'category': category,
+        'price': price,
+        'amount': amount
+    }
+    invoice.get('details').append(details)
+    answer = input('do you wish to continue? yes/no')
+
+    if answer == 'no':
+        state = False
+
